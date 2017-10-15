6544960cc121723f1471aae13c12341068beefaa
Shohei Kameda <shoheik@cpan.org>
2017-10-15 10:46:03 +0900

    changed to login

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 # create user
 useradd -m -d /home/$USERNAME -u 7777 -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USERNAME
 usermod -aG sudo $USERNAME
-cd /home/$USERNAME
-ttyd -u 7777 -p 8080 /bin/bash &
+
+# cd /home/$USERNAME
+#ttyd -u 7777 -p 8080 /bin/bash &  # run bash
+ttyd -u 7777 -p 8080 login & # login prompt - auth
 
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password

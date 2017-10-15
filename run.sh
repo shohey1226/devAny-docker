4de68c558596c5633776a0079fba1cb86fede06c
Shohei Kameda <shoheik@cpan.org>
2017-10-15 13:55:35 +0900

    added basic

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@
 useradd -m -d /home/$USERNAME -u 7777 -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USERNAME
 usermod -aG sudo $USERNAME
 
-# cd /home/$USERNAME
-#ttyd -u 7777 -p 8080 /bin/bash &  # run bash
-ttyd -u 7777 -p 8080 login & # login prompt - auth
+cd /home/$USERNAME
+ttyd -c $USERNAME:$PASSWORD -u 7777 -p 8080 /bin/bash &  
 
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
 chown root:www-data /etc/apache2/webdav.password

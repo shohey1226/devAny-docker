dd4dcdd68044411dc85600c8750d8a55789f429e
Shohei Kameda <shoheik@cpan.org>
2017-10-18 13:36:23 +0900

    apache now runs by userid

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ cd /home/$USERNAME
 ttyd -c $USERNAME:$PASSWORD -u 7777 -p 8080 /bin/bash &  
 
 htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
-chown root:www-data /etc/apache2/webdav.password
+chown root:$USERNAME /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
 
-apache2 -D FOREGROUND 
+APACHE_RUN_USER=$USERNAME APACHE_RUN_GROUP=$USERNAME apache2 -D FOREGROUND
 
